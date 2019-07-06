df737ed50845163ac14cddeee5bf7d8342f3013a
sway9119 <fear.and.sway@gmail.com>
2019-07-06 23:56:23 +0900

    update-main_activity

--- app/src/main/java/com/example/eggsnaky/MainActivity.java
+++ app/src/main/java/com/example/eggsnaky/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.eggsnaky;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textViewOfBrilliandroid);
+        textView.setBackgroundColor(Color.DKGRAY);
     }
 }
